015e0d254b5cc4fc2271e629e310983e0a52ca3f
abhi9871 <at719273@gmail.com>
2024-03-06 08:41:50 +0530

    Implementing Login

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -17,16 +17,19 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordRef.current.value;
 
+    setIsLoading(true);
+
+    let url;
+
     if(isLogin){
-      setIsLoading(true);
-      setTimeout(() => {
-        console.log('Login successfully');
-        setIsLoading(false);
-      }, 500)
-    } else{
-     try {
-        setIsLoading(true);
-        let response =  await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAjx4FhYqPFnze6jTZ9z7FSAO1We42aynQ',{
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAjx4FhYqPFnze6jTZ9z7FSAO1We42aynQ';
+    }
+    else {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAjx4FhYqPFnze6jTZ9z7FSAO1We42aynQ';
+    }
+
+    try {
+        let response =  await fetch(url,{
           method: 'POST',
           body: JSON.stringify({
             email: enteredEmail,
@@ -37,53 +40,57 @@ const AuthForm = () => {
             'Content-Type': 'application/json'
           }
      })
+
+        setIsLoading(false);
+
         if(response.ok){
-          setIsLoading(false);
+          console.log(await response.json());
         }
         else {
-          let err = await response.json();
-          throw new Error(err.error.message);
+          let errorMessage = 'Authentication failed...'
+          let data = await response.json();
+          if(data && data.error && data.error.message){
+              errorMessage =  data.error.message;
+          }
+            throw new Error(errorMessage);
         }
      } catch (error) {
         setIsLoading(false);
         alert(error.message);
-        console.log(error);
      }
     }
+    return (
+      <section className={classes.auth}>
+        <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+        <form onSubmit={submitHandler} >
+          <div className={classes.control}>
+            <label htmlFor='email'>Your Email</label>
+            <input type='email' id='email' ref={emailInputRef} required />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor='password'>Your Password</label>
+            <input
+              type='password'
+              id='password'
+              ref={passwordRef}
+              required
+            />
+          </div>
+          <div className={classes.actions}>
+          <button>
+              {isLoading ? 'Loading...' : (isLogin ? 'Login' : 'Create new account')}
+            </button>
+            <button
+              type='button'
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? 'Create new account' : 'Login with existing account'}
+            </button>
+          </div>
+        </form>
+      </section>
+    );
   }
-
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={submitHandler} >
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' ref={emailInputRef} required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input
-            type='password'
-            id='password'
-            ref={passwordRef}
-            required
-          />
-        </div>
-        <div className={classes.actions}>
-        <button>
-            {isLoading ? 'Loading...' : (isLogin ? 'Login' : 'Create new account')}
-          </button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
-};
 
 export default AuthForm;
